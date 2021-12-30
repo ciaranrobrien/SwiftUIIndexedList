7f409b83387772fa57523555d5d867db35a4116b
Ciaran O'Brien <ciaranrobrien@gmail.com>
2021-12-30 08:50:21 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IndexedScrollView
+
+A description of this package.
---
+++ Sources/IndexedScrollView/IndexedScrollView.swift
@@ -0,0 +1,6 @@
+public struct IndexedScrollView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
